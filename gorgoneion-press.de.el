25bbfd145bc71667aeb4af71a34b26be988239ba
Bastian Brinkmann <bookhacker@bookhacker.org>
2017-11-12 22:31:37 +0100

    Index Achaia hinzugefügt.

--- gorgoneion-press.de.el
+++ gorgoneion-press.de.el
@@ -112,7 +112,7 @@ relative link in the sitemap-publish function"
 ;;; ---------------------------------------------------------
 ;;; 
 (setq org-publish-project-gorgoneion-press
-      '(("gorgoneion-press" :components ("gorgoneion-press-articles" "gorgoneion-press-pages" "gorgoneion-press-static"))
+      '(("gorgoneion-press" :components ("gorgoneion-press-articles" "gorgoneion-press-pages" "gorgoneion-press-index-achaia" "gorgoneion-press-static"))
 	("gorgoneion-press-articles"
  	 :base-directory "~/org/websites/gorgoneion-press.de/org-files/blog/"
 	 :base-extension "org"
@@ -204,7 +204,46 @@ relative link in the sitemap-publish function"
 <p>Der Verlag für antike Fantasy und alternative Mythologie.</p>
 </div>
 </div>
-</footer>")	
+</footer>")
+	("gorgoneion-press-index-achaia"
+ 	 :base-directory "~/org/websites/gorgoneion-press.de/org-files/achaia/"
+	 :base-extension "org"
+	 :publishing-directory "~/org/websites/gorgoneion-press.de/public_html/achaia/"
+	 :recursive nil
+	 :publishing-function org-html-publish-to-html
+	 :with-author t
+	 :with-creator nil
+	 :with-date t
+	 :headline-level 4
+	 :section-numbers nil
+	 :with-toc nil
+	 :with-drawers t
+	 :html-link-home "/"
+	 :html-preamble
+	 "<header id=\"banner\">
+<div class=\"wrapper\">
+  <h1><a href=\"../index.html\">GORGONEION <i>Press</i></a></h1>
+  <nav><ul>
+    <li><a href=\"../datenschutz.html\">Datenschutz</a></li>
+    <li><a href=\"../impressum.html\">Impressum</a></li>
+  </ul></nav>
+</div>
+</header>"	 
+	 :html-postamble
+	 "<footer>
+<div class=\"wrapper\">
+	 <h2 class=\"footer-heading\">GORGONEION <i>Press</i></h2>
+<div class=\"footer-left\">
+<ul class=\"contact-list\">
+<li>GORGONEION <i>Press</i></li>
+<li><a href=\"mailto:kontakt@gorgoneion-press.de\">kontakt@gorgoneion-press.de</a></li>
+</ul>
+</div>
+<div class=\"footer-right\">
+<p>Der Verlag für antike Fantasy und alternative Mythologie.</p>
+</div>
+</div>
+</footer>")
 	("gorgoneion-press-static"
 	 :base-directory "~/org/websites/gorgoneion-press.de/org-files/"
 	 :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|epub\\|mobi"
